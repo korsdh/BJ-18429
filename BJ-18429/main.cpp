e3ddf80fade60563e208cdf3808b1abb09cd440c
korsdh@naver.com <korsdh@naver.com>
2022-09-14 21:22:03 +0900

    초기 커밋.

---
+++ BJ-18429/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int n, k;
+int cnt = 0;
+int arr[9];
+int now = 500;
+int res[9];
+bool visit[9];
+
+void trace(int day) {
+	if (day >= 1) {
+		now -= k;
+		if (now < 500) return;
+	}
+	if (day == n) {
+		cnt++;
+		return;
+	}
+	for (int i = 0; i < n; i++) {
+		if (visit[i] == true) continue;
+		visit[i] = true;
+		res[day] = arr[i];
+		now += res[day];
+		trace(day + 1);
+		visit[i] = false;
+	}
+}
+
+int main() {
+	cin >> n >> k;
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+	trace(0);
+	cout << cnt << endl;
+	return 0;
+}
